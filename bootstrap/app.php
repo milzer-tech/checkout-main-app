e16e1d131106f61b457fb8316078ab29f7442e33
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-12-10 14:09:49 +0100

    chore: add CSRF token validation exception for livewire routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'livewire/*',
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
